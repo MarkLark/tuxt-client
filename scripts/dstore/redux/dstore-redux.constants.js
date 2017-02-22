4889be5366fdae007e8e7c58cc2a918936875d91
MarkLark <mark.pittaway@mlit.net.au>
2017-02-22 14:56:54 +1100

    Client: Fixed lint issues with scripts/dstore/redux/dstore-redux.constants.js

--- scripts/dstore/redux/dstore-redux.constants.js
+++ scripts/dstore/redux/dstore-redux.constants.js
@@ -1,23 +1,23 @@
 export const CALL_API = 'Call API';
 
 export const ACTIONS = {
-    ADD: "ADD",
-    READ: "READ",
-    READ_ALL: "READ_ALL",
-    UPDATE: "UPDATE",
-    DELETE: "DELETE"
-}
+    ADD: 'ADD',
+    READ: 'READ',
+    READ_ALL: 'READ_ALL',
+    UPDATE: 'UPDATE',
+    DELETE: 'DELETE'
+};
 
 export const HTTP_METHODS = {
-    ADD: "POST",
-    READ: "GET",
-    READ_ALL: "GET",
-    UPDATE: "PATCH",
-    DELETE: "DELETE"
-}
+    ADD: 'POST',
+    READ: 'GET',
+    READ_ALL: 'GET',
+    UPDATE: 'PATCH',
+    DELETE: 'DELETE'
+};
 
 export const TYPES = {
-    REQUEST: "REQUEST",
-    SUCCESS: "SUCCESS",
-    FAILURE: "FAILURE"
-}
+    REQUEST: 'REQUEST',
+    SUCCESS: 'SUCCESS',
+    FAILURE: 'FAILURE'
+};
